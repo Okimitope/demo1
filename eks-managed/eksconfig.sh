3ce86e60692a7af44ca41c17540b444a71c601dc
rajaakeju4real@yahoo.com <rajaakeju4real@yahoo.com>
2023-02-14 19:58:26 -0500

    1

--- eks-managed/eksconfig.sh
+++ eks-managed/eksconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 path=$(pwd)
 rm -rf eks-config.yml aws-auth.yml
-cluster_name="eks-cluster"
+cluster_name="eks-demo"
 aws eks update-kubeconfig --region us-west-2 --name ${cluster_name} --profile joyoushire --kubeconfig=${path}/eks-config.yml
 kubectl  --kubeconfig=${path}/eks-config.yml  get -n kube-system configmap/aws-auth -o  yaml >> ${path}/aws-auth.yml
